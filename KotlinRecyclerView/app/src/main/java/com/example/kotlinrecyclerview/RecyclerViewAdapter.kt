44e1827004437b50da70bf66ead7b1bd9b6c77b2
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-08-14 11:30:59 +0700

    Implement KTX

--- KotlinRecyclerView/app/src/main/java/com/example/kotlinrecyclerview/RecyclerViewAdapter.kt
+++ KotlinRecyclerView/app/src/main/java/com/example/kotlinrecyclerview/RecyclerViewAdapter.kt
@@ -5,12 +5,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.view.*
 
 class RecyclerViewAdapter (private val context : Context, private val items : List<Item>, private val listener : (Item) -> Unit) :
-        RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+        RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
             ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
@@ -22,17 +22,12 @@ class RecyclerViewAdapter (private val context : Context, private val items : Li
     override fun getItemCount(): Int = items.size
 
 
-    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-
-        private val name = view.findViewById<TextView>(R.id.name)
-        private val image = view.findViewById<ImageView>(R.id.image)
+    class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView) , LayoutContainer {
 
         fun bindItem(items : Item, listener: (Item) -> Unit){
-            name.text = items.name
-            items.image?.let { Picasso.get().load(it).fit().into(image) }
-            itemView.setOnClickListener{
-                listener(items)
-            }
+            itemView.name.text = items.name
+            items.image?.let { Picasso.get().load(it).fit().into(itemView.image) }
+            containerView.setOnClickListener{ listener(items) }
         }
     }
 }

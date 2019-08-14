bc73dedad51c93b063e9331d0f4b2a85537a1ebd
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-08-14 11:36:20 +0700

    Implement Kotlin Android Extensions

--- KotlinRecyclerView/app/src/main/java/com/example/kotlinrecyclerview/RecyclerViewAdapter.kt
+++ KotlinRecyclerView/app/src/main/java/com/example/kotlinrecyclerview/RecyclerViewAdapter.kt
@@ -5,9 +5,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.view.*
 
 class RecyclerViewAdapter (private val context : Context, private val items : List<Item>, private val listener : (Item) -> Unit) :
         RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
@@ -22,15 +22,12 @@ class RecyclerViewAdapter (private val context : Context, private val items : Li
     override fun getItemCount(): Int = items.size
 
 
-    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-
-        private val name = view.findViewById<TextView>(R.id.name)
-        private val image = view.findViewById<ImageView>(R.id.image)
+    class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
         fun bindItem(items : Item, listener: (Item) -> Unit){
-            name.text = items.name
-            items.image?.let { Picasso.get().load(it).fit().into(image) }
-            itemView.setOnClickListener{
+            itemView.name.text = items.name
+            items.image?.let { Picasso.get().load(it).fit().into(itemView.image) }
+            containerView.setOnClickListener{
                 listener(items)
             }
         }

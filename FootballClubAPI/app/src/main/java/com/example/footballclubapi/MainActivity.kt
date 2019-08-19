f14d9f27b56c52dd6631af042fa9e880b0c00608
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-08-19 11:50:30 +0700

    Create layout

---
+++ FootballClubAPI/app/src/main/java/com/example/footballclubapi/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.footballclubapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.footballclubapi.R.color.colorAccent
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var listTeam : RecyclerView
+    private lateinit var progressBar : ProgressBar
+    private lateinit var swipeRefresh : SwipeRefreshLayout
+    private lateinit var spinner : Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            spinner = spinner()
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(
+                    colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light)
+
+                relativeLayout {
+                    lparams(width = matchParent, height = wrapContent)
+
+                    listTeam = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(context)
+                    }
+
+                    progressBar = progressBar {
+                    }.lparams{
+                        centerHorizontally()
+                    }
+                }
+            }
+        }
+
+
+    }
+}

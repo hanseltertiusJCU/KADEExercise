c69094cc82ae4a14251fe1dc8e7fb4dfa8804da3
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-09-03 21:41:57 +0700

    Implement display favorite team on Fragment

--- FootballClubAPI/app/src/main/java/com/example/footballclubapi/fragment/FavoriteTeamsFragment.kt
+++ FootballClubAPI/app/src/main/java/com/example/footballclubapi/fragment/FavoriteTeamsFragment.kt
@@ -1,24 +1,90 @@
 package com.example.footballclubapi.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.footballclubapi.R
+import com.example.footballclubapi.activity.TeamDetailActivity
+import com.example.footballclubapi.adapter.FavoriteTeamsAdapter
+import com.example.footballclubapi.helper.database
+import com.example.footballclubapi.model.Favorite
+import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.select
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.onRefresh
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
-/**
- * A simple [Fragment] subclass.
- */
-class FavoriteTeamsFragment : Fragment() {
+
+class FavoriteTeamsFragment : Fragment(), AnkoComponent<Context> {
+
+    private var favorites : MutableList<Favorite> = mutableListOf()
+    private lateinit var adapter : FavoriteTeamsAdapter
+    private lateinit var listTeam : RecyclerView
+    private lateinit var swipeRefresh : SwipeRefreshLayout
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        adapter = FavoriteTeamsAdapter(favorites){
+            context?.startActivity<TeamDetailActivity>("id" to "${it.teamId}")
+        }
+
+        listTeam.adapter = adapter
+        swipeRefresh.onRefresh {
+            showFavorite()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showFavorite()
+    }
+
+    private fun showFavorite(){
+        favorites.clear()
+        context?.database?.use {
+            swipeRefresh.isRefreshing = false
+            val result = select(Favorite.TABLE_FAVORITE)
+            val favorite = result.parseList(classParser<Favorite>())
+            favorites.addAll(favorite)
+            adapter.notifyDataSetChanged()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return TextView(activity).apply {
+        return createView(AnkoContext.create(requireContext()))
+    }
+
+    override fun createView(ui: AnkoContext<Context>): View = with(ui){
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(R.color.colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light)
+
+                listTeam = recyclerView {
+                    lparams(width = matchParent, height = wrapContent)
+                    layoutManager = LinearLayoutManager(ctx)
+                }
+            }
         }
     }
 

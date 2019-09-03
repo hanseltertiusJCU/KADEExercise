494c1d0deeb414bd88afac0a5759186f29b34c08
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-09-03 22:12:11 +0700

    Implement remove team from database

--- FootballClubAPI/app/src/main/java/com/example/footballclubapi/activity/TeamDetailActivity.kt
+++ FootballClubAPI/app/src/main/java/com/example/footballclubapi/activity/TeamDetailActivity.kt
@@ -2,8 +2,8 @@ package com.example.footballclubapi.activity
 
 import android.database.sqlite.SQLiteConstraintException
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
@@ -11,6 +11,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.footballclubapi.R
@@ -25,31 +26,40 @@ import com.example.footballclubapi.view.TeamDetailView
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.delete
 import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
 
-    private lateinit var progressBar : ProgressBar
-    private lateinit var swipeRefresh : SwipeRefreshLayout
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
 
-    private lateinit var teamBadge : ImageView
-    private lateinit var teamName : TextView
-    private lateinit var teamFormedYear : TextView
-    private lateinit var teamStadium : TextView
-    private lateinit var teamDescription : TextView
+    private lateinit var teamBadge: ImageView
+    private lateinit var teamName: TextView
+    private lateinit var teamFormedYear: TextView
+    private lateinit var teamStadium: TextView
+    private lateinit var teamDescription: TextView
 
-    private lateinit var presenter : TeamDetailPresenter
-    private lateinit var teams : Team
-    private lateinit var id : String
+    private lateinit var presenter: TeamDetailPresenter
+    private lateinit var teams: Team
+    private lateinit var id: String
 
-    private var menuItem : Menu? = null
-    private var isFavorite : Boolean = false
+    private var menuItem: Menu? = null
+    private var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val intent = intent
+        id = intent.getStringExtra("id")
+        Log.d("id", id)
+        supportActionBar?.title = "Team Detail"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         linearLayout {
             lparams(width = matchParent, height = wrapContent)
@@ -57,10 +67,12 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
             backgroundColor = Color.WHITE
 
             swipeRefresh = swipeRefreshLayout {
-                setColorSchemeResources(R.color.colorAccent,
+                setColorSchemeResources(
+                    R.color.colorAccent,
                     android.R.color.holo_green_light,
                     android.R.color.holo_orange_light,
-                    android.R.color.holo_red_light)
+                    android.R.color.holo_red_light
+                )
 
                 scrollView {
                     isVerticalScrollBarEnabled = false
@@ -82,7 +94,7 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
                                     this.gravity = Gravity.CENTER
                                     textSize = 20f
                                     textColor = ContextCompat.getColor(context, R.color.colorAccent)
-                                }.lparams{
+                                }.lparams {
                                     topMargin = dip(5)
                                 }
 
@@ -90,18 +102,19 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
                                     this.gravity = Gravity.CENTER
                                 }
 
-                                teamStadium = textView{
+                                teamStadium = textView {
                                     this.gravity = Gravity.CENTER
-                                    textColor = ContextCompat.getColor(context, R.color.colorPrimaryText)
+                                    textColor =
+                                        ContextCompat.getColor(context, R.color.colorPrimaryText)
                                 }
 
-                                teamDescription = textView().lparams{
+                                teamDescription = textView().lparams {
                                     topMargin = dip(20)
                                 }
                             }
 
-                            progressBar = progressBar{
-                            }.lparams{
+                            progressBar = progressBar {
+                            }.lparams {
                                 centerHorizontally()
                             }
 
@@ -111,15 +124,12 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
             }
         }
 
-        supportActionBar?.title = "Team Detail"
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        val intent = intent
-        id = intent.getStringExtra("id")
-        val request =  ApiRepository()
+        favoriteState()
+        val request = ApiRepository()
         val gson = Gson()
         presenter = TeamDetailPresenter(this, request, gson)
         presenter.getTeamDetail(id)
+
         swipeRefresh.onRefresh {
             presenter.getTeamDetail(id)
         }
@@ -147,34 +157,73 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.detail_menu, menu)
         menuItem = menu
+        setFavorite()
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
+        return when (item.itemId) {
             android.R.id.home -> {
                 finish()
                 true
             }
             R.id.add_to_favorite -> {
-                addToFavorite()
+                if(isFavorite) removeFromFavorite() else addToFavorite()
+
+                isFavorite = !isFavorite
+                setFavorite()
+
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun addToFavorite(){
-        try{
-            database.use{
-                insert(Favorite.TABLE_FAVORITE,
-                    Favorite.TEAM_ID to teams.teamName,
+    private fun addToFavorite() {
+        try {
+            database.use {
+                insert(
+                    Favorite.TABLE_FAVORITE,
+                    Favorite.TEAM_ID to teams.teamId,
                     Favorite.TEAM_NAME to teams.teamName,
-                    Favorite.TEAM_BADGE to teams.teamBadge)
+                    Favorite.TEAM_BADGE to teams.teamBadge
+                )
             }
             swipeRefresh.snackbar("Added to Favorite").show()
-        } catch (e : SQLiteConstraintException){
+        } catch (e: SQLiteConstraintException) {
             swipeRefresh.snackbar(e.localizedMessage).show()
+        }
+    }
+
+    private fun removeFromFavorite() {
+        try {
+            database.use {
+                delete(
+                    Favorite.TABLE_FAVORITE,
+                    "(TEAM_ID = {id})",
+                    "id" to id
+                )
+            }
+            swipeRefresh.snackbar("Removed from favorite").show()
+        } catch (e: SQLiteConstraintException) {
+            swipeRefresh.snackbar(e.localizedMessage).show()
+        }
+    }
+
+    private fun setFavorite(){
+        if(isFavorite){
+            menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, R.drawable.ic_added_to_favorites)
+        } else {
+            menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, R.drawable.ic_add_to_favorites)
+        }
+    }
+
+    private fun favoriteState(){
+        database.use {
+            val result = select(Favorite.TABLE_FAVORITE)
+                .whereArgs("(TEAM_ID = {id})", "id" to id)
+            val favorite = result.parseList(classParser<Favorite>())
+            if(favorite.isNotEmpty()) isFavorite = true
         }
     }
 }
